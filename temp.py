84ced009f077f5844a65261b3edb509001e4113a
stw <2990834217@qq.com>
2025-02-12 19:22:21 +0800

    update Elasticsearch file

--- temp.py
+++ temp.py
@@ -1,26 +1,44 @@
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-import spacy
+import pandas as pd
+from elasticsearch import Elasticsearch
+import os
+import time
 
-nlp = spacy.load("en_core_web_sm")
 
-documents = ('In March 1977, a large volume of the industrial chemical hexachlorocyclopentadiene (HCCPD) was dumped into '
-             'a municipal sewage system in Kentucky. We evaluated the health effects of exposure to HCCPD in '
-             '145 sewage treatment plant workers. We found that 85 (59%) had noted eye irritation, 65 (45%) had headaches, '
-             'and 39 (27%) had throat irritation. Symptoms occurred throughout the plant; however, highest attack rates '
-             'occurred in primary sewage treatment areas. Medical examination of 41 employees three days after the plant'
-             ' was closed showed proteinuria and elevation of serum lactic dehydrogenase levels; these findings were not '
-             'present three weeks later. This episode demonstrates the toxicity of HCCPD and emphasizes the vulnerability '
-             'of sewage workers to chemical toxins in wastewater systems.')
-doc = nlp(documents)
+# 配置Elasticsearch连接
+es = Elasticsearch(
+    ['http://localhost:9200/']
+)
+# 定义索引名称
+index_name = "abstract22"
 
-genes = []
-diseases = []
+folder_path = "D:/Snoopy/result"
+base_filename = "pubmed25n"
+start_num = 1016
+end_num = 1275
 
-for ent in doc.ents:
-    if ent.label_ == "CHEMICAL":  # 假设病名被识别为地理位置（GPE）
-        diseases.append(ent.text)
-    elif ent.label_ == "ORG":  # 假设基因名被识别为组织（ORG）
-        genes.append(ent.text)
+print("start parsing")
+for num in range(start_num, end_num + 1):
+    start_time = time.time()
+    num_str = f"{num:04d}"
+    file_path = os.path.join(folder_path, f"{base_filename}{num_str}.xlsx")
+    df = pd.read_excel(file_path)
+    data_to_insert = df[["PMID", "Abstract"]].rename(columns={"PMID": "pubMedId", "Abstract": "abstract"}).to_dict(orient="records")
 
-print("Genes:", genes)
-print("Diseases:", diseases)
+    for idx, item in enumerate(data_to_insert, start=1):
+        try:
+            response = es.index(index=index_name, body=item)
+            print(f"第 {idx} 条数据插入成功")
+        except Exception as e:
+            print(f"第 {idx} 条数据插入失败：", e)
+
+    # 验证数据
+    query = {
+        "query": {
+            "match_all": {}
+        }
+    }
+
+    response = es.search(index=index_name, body=query)
+    print("查询结果：", response)
+
+    print(f"第 {num} 个xml文件插入成功")
